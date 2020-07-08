0f28f579f413e6519a99dd746ae1092753d1f149
Vasiliy Fedichkin <36080408+saimonis@users.noreply.github.com>
2020-07-08 16:37:51 +0300

    Edit works

--- src/notes_part/reducers.ts
+++ src/notes_part/reducers.ts
@@ -1,38 +1,53 @@
 interface INotesDefaultState {
-  payload: any;
+  data: any;
   loading: boolean;
 }
 
-const notesDefaultState: INotesDefaultState = { payload: [], loading: true };
+const notesDefaultState: INotesDefaultState = { data: [], loading: true };
 
 export const notes = (state: any = notesDefaultState, action: any) => {
   switch (action.type) {
     case "FETCHING_DATA":
       return {
-        payload: [...action.payload],
+        data: [...action.payload],
         loading: false,
       };
     case "ADD_NOTE":
       return {
-        payload: [...state.payload, { ...action.payload }],
+        data: [...state.data, { ...action.payload }],
+      };
+    case "PATCH_ITEM_PROGRESS":
+      return {
+        ...state,
+        data: state.data.map((i: any) => {
+          if (i.id === action.payload.id) {
+            return action.payload;
+          }
+          return i;
+        }),
       };
     default:
       return state;
   }
 };
 
-const formDefaultState: any = { loading: false };
+interface IFormDefaultState {
+  loading: boolean;
+  item: boolean | object;
+}
+
+const formDefaultState: IFormDefaultState = { item: false, loading: false };
 
 export const form = (state = formDefaultState, action: any) => {
   switch (action.type) {
     case "ADD_NOTE":
-      return {
-        loading: true,
-      };
-    case "ADD_NOTE_SUCCESS":
-      return {
-        loading: false,
-      };
+      return { ...state, loading: true };
+    case "NOTE_SUCCESS":
+      return { ...state, item: false, loading: false };
+    case "CHANGING_DATA_ITEM_START":
+      return { ...state, item: { ...action.playload } };
+    case "PATCH_ITEM_PROGRESS":
+      return { ...state, loading: true };
     default:
       return state;
   }
